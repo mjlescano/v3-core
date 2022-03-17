99c2eec7977356aa2c7d966b1090da6200ff8e19
Matías <mjlescano@protonmail.com>
2022-03-16 21:46:06 -0600

    add stuff

--- contracts/CannonImports.sol
+++ contracts/CannonImports.sol
@@ -2,4 +2,6 @@
 pragma solidity =0.7.6;
 
 import {SwapRouter} from "@uniswap/v3-periphery/contracts/SwapRouter.sol";
+import {NonfungiblePositionManager} from "@uniswap/v3-periphery/contracts/NonfungiblePositionManager.sol";
+import "@uniswap/v3-periphery/contracts/NonfungibleTokenPositionDescriptor.sol";
 import {QuoterV2} from "@uniswap/v3-periphery/contracts/lens/QuoterV2.sol";
